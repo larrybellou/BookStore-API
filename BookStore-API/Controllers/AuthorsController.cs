7319c177beca1edb12f83b7b2c21c2501abed605
Larry Bellou <LarryBellou@msn.com>
2021-05-27 15:51:03 -0500

    create author added

--- BookStore-API/Controllers/AuthorsController.cs
+++ BookStore-API/Controllers/AuthorsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BookStore_API.Contracts;
+using BookStore_API.Data;
 using BookStore_API.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -31,11 +32,14 @@ namespace BookStore_API.Controllers
             _mapper = mapper;
         }
 
+        #region "Gets"
         /// <summary>
         /// Get all authors
         /// </summary>
         /// <returns>List of Authors</returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAuthors()
         {
             try
@@ -57,6 +61,9 @@ namespace BookStore_API.Controllers
         /// </summary>
         /// <returns>List of Authors</returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAuthor(int id)
         {
             try
@@ -77,6 +84,50 @@ namespace BookStore_API.Controllers
                 return internalError($"Error Get Author call occurred for id:{id}: " + e.Message);
             }
         }
+        #endregion
+
+        #region "Create"
+        public async Task<IActionResult> Create([FromBody] AuthorCreateDTO authorDTO)
+        {
+            try
+            {
+                if(authorDTO == null)
+                {
+                    _logger.LogWarn($"Create author failed with empty author.");
+                    return BadRequest(ModelState);
+                }
+                if(!ModelState.IsValid)
+                {
+                    _logger.LogWarn($"Create author failed with missing fields for author.");
+                    return BadRequest(ModelState);
+                }
+                _logger.LogInfo($"Attempting Create Author call for {authorDTO.Firstname}-{authorDTO.Lastname}");
+                var author = _mapper.Map<Author>(authorDTO);
+
+                var isSuccess = await _autherRepository.Create(author);
+                if(!isSuccess)
+                {
+                    return internalError($"Create author failed on save.");
+                }
+
+                return Created("Created", author);
+
+                //var author = await _autherRepository.FindById(id);
+                //if (author == null)
+                //{
+                //    _logger.LogWarn($"Get Author call for id:{id} was not found.");
+                //    return NotFound();
+                //}
+                //var response = _mapper.Map<IList<AuthorDTO>>(author);
+                //_logger.LogInfo($"Success Get Author call for id:{id}");
+                //return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return internalError($"Error Creating Author");
+            }
+        }
+        #endregion
 
         private ObjectResult internalError(string message)
         {
