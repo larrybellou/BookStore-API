caff257ca83148388b6b617651d1233fae7ac730
Larry Bellou <larrybellou@gmail.com>
2021-05-28 15:27:27 -0500

    book fixes

--- BookStore-API/DTOs/BookDTO.cs
+++ BookStore-API/DTOs/BookDTO.cs
@@ -14,7 +14,7 @@ namespace BookStore_API.DTOs
         public string Isbn { get; set; }
         public string Summary { get; set; }
         public string Image { get; set; }
-        public double? Price { get; set; }
+        public decimal? Price { get; set; }
 
         public int? AuthorId { get; set; }
         public virtual AuthorDTO Author { get; set; }
@@ -27,9 +27,10 @@ namespace BookStore_API.DTOs
         public int? Year { get; set; }
         [Required]
         public string Isbn { get; set; }
+        [StringLength(500)]
         public string Summary { get; set; }
         public string Image { get; set; }
-        public double? Price { get; set; }
+        public decimal? Price { get; set; }
         [Required]
         public int AuthorId { get; set; }
     }
@@ -40,13 +41,11 @@ namespace BookStore_API.DTOs
         [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
-        [Required]
-        public string Isbn { get; set; }
         [StringLength(500)]
         public string Summary { get; set; }
         public string Image { get; set; }
-        public double? Price { get; set; }
+        public decimal? Price { get; set; }
         [Required]
-        public int AuthorId { get; set; }
+        public int? AuthorId { get; set; }
     }
 }
